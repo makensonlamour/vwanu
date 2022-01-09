35646320cd59bb4be0be4b70263e938388aceb0f
Yves Gervens Constant <gervoos@gmail.com>
2022-01-09 11:18:39 -0500

    :tada: basic files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <header className="">
+        <h1 className="">VWANU Social Media</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
