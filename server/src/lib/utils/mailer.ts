cbdfa0aa41259e1c1be22395f2e918909cf52f73
Wadson Vaval <vavalson@hotmail.com>
2022-02-19 19:58:18 -0400

    continuous development and deployment
    * changed event name
    
    * some chabged
    
    * continous integation
    
    * some,
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * some
    
    * workflow created
    
    * fix: unessacery scripts runing on deployment
    
    * deployment
    
    * fixed docker copy issue
    
    * some

--- server/src/lib/utils/mailer.ts
+++ server/src/lib/utils/mailer.ts
@@ -1,29 +1,37 @@
-import config from 'config'
-import nodemailer, { SendMailOptions } from 'nodemailer'
-import Log from './logger'
+// import config from 'config';
+import nodemailer, { SendMailOptions } from 'nodemailer';
+import Log from './logger';
 
-const smtp = config.get<{
-  user: string
-  pass: string
-  host: string
-  port: number
-  secure: boolean
-}>('smtp')
+// const smtp = config?.get<{
+//   user: string;
+//   pass: string;
+//   host: string;
+//   port: number;
+//   secure: boolean;
+// }>('smtp')  
+
+const smtp ={
+  user: 'mzghhfqe244g3ssi@ethereal.email',
+  pass: 'VGwXCd8dr3xZDNTXuZ',
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false,
+};
 
 const transporter = nodemailer.createTransport({
   ...smtp,
   auth: { user: smtp.user, pass: smtp.pass },
-})
+});
 
 async function sendEmail(payload: SendMailOptions) {
   transporter.sendMail(payload, (err, info) => {
     if (err) {
-      Log.error('Error sending email')
-      return
+      Log.error('Error sending email');
+      return;
     }
 
-    Log.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`)
-  })
+    Log.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+  });
 }
 
-export default sendEmail
+export default sendEmail;
