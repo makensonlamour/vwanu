198255d3ad18c1f374689132d404d38e45cc1792
Wadson <vavalson@hotmail.com>
2022-03-11 10:45:15 -0400

    refactor(usermodel and userschema): add more attribute to user model and schema
    Add more attributes into the user to make it replace the profile model
    
    BREAKING CHANGE: Can no longer create a user without specifiying his first and last name.

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,14 +6,14 @@ import { Model } from 'sequelize';
 // Custom imports
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { UserInterface } from '../schema/user';
+import { UpUserInterface as UserInterface } from '../schema/user';
 import createToken from '../lib/utils/createToken';
 
 module.exports = (sequelize: any, DataTypes: any) => {
   class User extends Model<UserInterface> implements UserInterface {
-    id?: number | undefined;
+    id: number | undefined;
 
-    email!: string;
+    email: string;
 
     activationKey?: string | null;
 
@@ -22,6 +22,76 @@ module.exports = (sequelize: any, DataTypes: any) => {
     verified?: boolean | undefined;
 
     password: string | undefined;
+
+    username?: string | undefined;
+
+    firstName: string;
+
+    lastName: string;
+
+    avatar: string | undefined;
+
+    cover: string;
+
+    backgroundImage: string;
+
+    backgroundImageStatus: boolean;
+
+    address: string;
+
+    working: string;
+
+    workingLink: string;
+
+    about: string;
+
+    school: string;
+
+    gender: string;
+
+    birthday: string;
+
+    countryId: number;
+
+    website: string;
+
+    facebook: string;
+
+    google: string;
+
+    twitter: string;
+
+    linkedin: string;
+
+    youtube: string;
+
+    vk: string;
+
+    instagram: string;
+
+    qq: string;
+
+    wechat: string;
+
+    discord: string;
+
+    mailru: string;
+
+    language: string;
+
+    followPrivacy: boolean;
+
+    friendPrivacy: boolean;
+
+    postPrivacy: string;
+
+    messagePrivacy: string;
+
+    lastSeenPrivacy: boolean;
+
+    active: boolean;
+
+    admin: boolean;
 
     static async setPassword(password: string): Promise<string> {
       const passwordHash = await bcrypt.hash(password, 12);
@@ -63,9 +133,32 @@ module.exports = (sequelize: any, DataTypes: any) => {
       },
       password: {
         type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      gender: {
+        type: DataTypes.STRING,
+        defaultValue: 'm',
+      },
+
+      about: {
+        type: DataTypes.STRING,
         allowNull: true,
       },
 
+      language: {
+        type: DataTypes.STRING,
+        defaultValue: 'en',
+      },
       activationKey: {
         type: DataTypes.STRING,
         allowNull: true,
@@ -78,6 +171,30 @@ module.exports = (sequelize: any, DataTypes: any) => {
       verified: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
+      },
+      active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+
+      lastSeenPrivacy: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+
+      admin: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+
+      cover: {
+        type: DataTypes.STRING,
+        defaultValue:
+          'https://images.unsplash.com/photo-1528464884105-28166ef8edd0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+      },
+      website: {
+        type: DataTypes.STRING,
+        allowNull: true,
       },
     },
     {
