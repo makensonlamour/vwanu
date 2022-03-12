52f39b0165774bc9a54aeb5996f3099b351bcfca
Wadson <vavalson@hotmail.com>
2022-03-12 19:44:10 -0400

    fix(requirelogin.ts): fix require login env dependency
    Require login was calling an env no longer in .env but in config folders
    
    fix 92

--- server/src/middleware/requireLogin/index.ts
+++ server/src/middleware/requireLogin/index.ts
@@ -1,35 +1,32 @@
-import jwt from 'jsonwebtoken'
-import { Response, Request, NextFunction } from 'express'
-import { StatusCodes, ReasonPhrases } from 'http-status-codes'
+import jwt from 'jsonwebtoken';
+import { Response, Request, NextFunction } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+import config from 'config';
 
-import common from '../../lib/utils/common'
+import common from '../../lib/utils/common';
 
-require('dotenv').config()
+const JWT_SECRET = config.get<string>('JWT_SECRET');
 
-const { catchAsync } = common
+const { catchAsync } = common;
 export default catchAsync(
   // eslint-disable-next-line consistent-return
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const token = req.header('x-auth-token')
+      const token = req.header('x-auth-token');
       if (!token)
         return next({
           message: StatusCodes.UNAUTHORIZED,
           status: ReasonPhrases.UNAUTHORIZED,
-        })
+        });
 
-      jwt.verify(
-        token,
-        process.env.JWT_SECRET as jwt.Secret,
-        (err, decoded) => {
-          if (err && !decoded)
-            return next({ ...err, status: StatusCodes.UNAUTHORIZED })
-          req.user = (<any>decoded).user
-          return next()
-        }
-      )
+      jwt.verify(token, JWT_SECRET as jwt.Secret, (err, decoded) => {
+        if (err && !decoded)
+          return next({ ...err, status: StatusCodes.UNAUTHORIZED });
+        req.user = (<any>decoded).user;
+        return next();
+      });
     } catch (e) {
-      return next({ ...e, status: StatusCodes.INTERNAL_SERVER_ERROR })
+      return next({ ...e, status: StatusCodes.INTERNAL_SERVER_ERROR });
     }
   }
-)
+);
