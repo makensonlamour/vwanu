50df4237923fe74b8125c2b58fd562822a08ac94
Wadson <vavalson@hotmail.com>
2022-05-12 17:58:53 -0400

    refactor(mailler): added mailler service

---
+++ server/src/services/mailer/mailler.service.test.ts
@@ -0,0 +1,28 @@
+import app from '../../app';
+import { generateFakeEmail } from '../../lib/utils/generateFakeUser';
+
+const userEmail = generateFakeEmail();
+const template = {
+  subject: 'subject',
+  body: '<p>This is the body of a great email</p> {link}',
+};
+
+describe('Mailer', () => {
+  it('should send an email', async () => {
+    const result = await app.service('mailer').create({
+      to: userEmail,
+      subject: template.subject,
+      html: template.body,
+    });
+
+    expect(result.response).toEqual(expect.stringContaining('250'));
+    expect(result.accepted).toEqual(expect.arrayContaining([userEmail]));
+
+    // const res = await axios.get(endpoint);
+    // const inbox = res.data;
+    // expect(inbox.result).toEqual('success');
+    // expect(inbox.emails[0].html).toEqual(
+    //   expect.stringContaining('This is the body of a great email')
+    // );
+  }, 40000);
+});
