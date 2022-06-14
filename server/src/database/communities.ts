b26a6212a8fbe1231465a62d2193b9a6d38af93b
Wadson <vavalson@hotmail.com>
2022-06-14 09:39:56 -0400

    feat(communities): fix community database
    added missing fields on community database

--- server/src/database/communities.ts
+++ server/src/database/communities.ts
@@ -9,6 +9,8 @@ export interface CommunityInterface {
 
   coverPicture: string;
 
+  description: string;
+
   profilePicture: string;
 
   privacyType: boolean;
@@ -18,6 +20,24 @@ export interface CommunityInterface {
   numMembers: number;
 
   numAdmins: number;
+
+  type: string; // public, hidden , private
+
+  canInvite: string; // all-member, organizers && Mods, org-only
+
+  canInPost: string; // all-member, organizers && Mods, org-only
+
+  canInUploadPhotos: string; // all-member, organizers && Mods, org-only
+
+  canInUploadDoc: string; // all-member, organizers && Mods, org-only
+
+  canInUploadVideo: string; // all-member, organizers && Mods, org-only
+
+  canMessageInGroup: string; // all-member, organizers && Mods, org-only
+
+  haveDiscussionForum: boolean;
+
+  defaultInvitationEmail: string;
 }
 export default (sequelize: any, DataTypes: any) => {
   class Community
@@ -30,6 +50,8 @@ export default (sequelize: any, DataTypes: any) => {
 
     coverPicture: string;
 
+    description: string;
+
     profilePicture: string;
 
     privacyType: boolean;
@@ -40,27 +62,45 @@ export default (sequelize: any, DataTypes: any) => {
 
     numAdmins: number;
 
+    type: string; // public, hidden , private
+
+    canInvite: string; // all-member, organizers && Mods, org-only
+
+    canInPost: string; // all-member, organizers && Mods, org-only
+
+    canInUploadPhotos: string; // all-member, organizers && Mods, org-only
+
+    canInUploadDoc: string; // all-member, organizers && Mods, org-only
+
+    canInUploadVideo: string; // all-member, organizers && Mods, org-only
+
+    canMessageInGroup: string; // all-member, organizers && Mods, org-only
+
+    haveDiscussionForum: boolean;
+
+    defaultInvitationEmail: string;
+
     static associate(models: any): void {
-      Community.belongsToMany(models.User, {
-        as: 'members',
-        through: 'community-members',
-      });
+      // Community.belongsToMany(models.User, {
+      //   as: 'members',
+      //   through: 'community-members',
+      // });
       Community.belongsToMany(models.User, {
         as: 'creator',
         through: 'community-creator',
       });
-      Community.belongsToMany(models.User, {
-        as: 'moderators',
-        through: 'community-moderators',
-      });
-      Community.belongsToMany(models.User, {
-        as: 'administrators',
-        through: 'community-administrators',
-      });
-      Community.belongsToMany(models.User, {
-        as: 'administratorsRequest',
-        through: 'community-administratorsRequest',
-      });
+      // Community.belongsToMany(models.User, {
+      //   as: 'moderators',
+      //   through: 'community-moderators',
+      // });
+      // Community.belongsToMany(models.User, {
+      //   as: 'administrators',
+      //   through: 'community-administrators',
+      // });
+      // Community.belongsToMany(models.User, {
+      //   as: 'administratorsRequest',
+      //   through: 'community-administratorsRequest',
+      // });
     }
   }
   Community.init(
@@ -88,9 +128,21 @@ export default (sequelize: any, DataTypes: any) => {
         allowNull: false,
         unique: true,
       },
+
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
       coverPicture: {
         type: DataTypes.STRING,
         allowNull: true,
+      },
+
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
       },
       profilePicture: {
         type: DataTypes.STRING,
@@ -99,6 +151,43 @@ export default (sequelize: any, DataTypes: any) => {
       privacyType: {
         type: DataTypes.STRING,
         allowNull: true,
+      },
+      canInvite: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      canInPost: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      canInUploadPhotos: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      canInUploadDoc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      canInUploadVideo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+
+      canMessageInGroup: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      defaultInvitationEmail: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+
+      haveDiscussionForum: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
       },
     },
 
