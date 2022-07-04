4b2e4820e8a73afb23bd2cace00a03973a8d4125
Wadson <vavalson@hotmail.com>
2022-07-04 04:40:35 -0400

    ci: remove file creation

--- server/src/sequelize.ts
+++ server/src/sequelize.ts
@@ -25,7 +25,7 @@ export default function (app: Application): void {
 
     // Set up data relationships
     // Sync to the database
-    app.set('sequelizeSync', sequelize.sync({ alter: true }));
+    app.set('sequelizeSync', sequelize.sync({ force: true }));
     return result;
   };
   function startSequelize() {
