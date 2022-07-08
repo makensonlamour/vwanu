a7d84ce91fe0588a2d470b7c8a112593b97ccbed
Yves Gervens Constant <gervoos@gmail.com>
2022-07-08 15:40:40 -0400

    fix(register): fix error show when a user try to register
    fix error show when a user try to register

--- client/src/pages/Profil/Steps/StepFour.jsx
+++ client/src/pages/Profil/Steps/StepFour.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import routesPath from "../../../routesPath";
 import Step from "./Step";
 
 const StepFour = () => {
@@ -10,9 +8,12 @@ const StepFour = () => {
         <Step step={4} className="step-primary" />
         <div className="w-1/2 m-auto">
           <h1 className="card-title text-orange-500">Find Friends</h1>
-          <Link className="btn btn-md btn-primary rounded-full text-base-100 mt-6 hover:bg-secondary" to={"../../" + routesPath.NEWSFEED}>
+          <button
+            onClick={() => window.location.reload()}
+            className="btn btn-md btn-primary rounded-full text-base-100 mt-6 hover:bg-secondary"
+          >
             Finish
-          </Link>
+          </button>
         </div>
       </div>
     </>
