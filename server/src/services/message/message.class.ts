0800eddf7f8b0e3310b31418f0d87c7954d33b05
Wadson <vavalson@hotmail.com>
2022-09-09 18:18:49 -0400

    feat(message media): added suport for message media

--- server/src/services/message/message.class.ts
+++ server/src/services/message/message.class.ts
@@ -1,10 +1,31 @@
+/* eslint-disable no-unused-vars */
+import { Params } from '@feathersjs/feathers';
 import { Service, SequelizeServiceOptions } from 'feathers-sequelize';
+
+/** Local dependencies */
+import common from '../../lib/utils/common';
+import { include } from '../../lib/utils/commentPostInclude';
 import { Application } from '../../declarations';
 
+const { getUploadedFiles } = common;
 // eslint-disable-next-line import/prefer-default-export
 export class Message extends Service {
+  app;
+
   // eslint-disable-next-line no-unused-vars
   constructor(options: Partial<SequelizeServiceOptions>, app: Application) {
     super(options);
+    this.app = app;
+  }
+
+  async create(data, params: Params) {
+    const messageData = getUploadedFiles(['messageImage'], data);
+    const message = await this.app
+      .service('message')
+      .Model.create(messageData, {
+        include: [{ model: this.app.get('sequelizeClient').models.Media }],
+      });
+
+    return Promise.resolve(message);
   }
 }
