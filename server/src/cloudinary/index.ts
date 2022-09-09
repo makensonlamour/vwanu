0f2597b030f02db3cea9208287c1b2493ddc1ddb
Wadson <vavalson@hotmail.com>
2022-09-09 18:17:13 -0400

    feat(added suport for message medias): add support for message media

--- server/src/cloudinary/index.ts
+++ server/src/cloudinary/index.ts
@@ -32,6 +32,15 @@ const postImages = new CloudinaryStorage({
     resource_type: 'auto',
   },
 });
+
+const messageMedia = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    allowedFormats: ['jpg', 'png', 'jpeg', 'gif', 'mp4', 'm4v'],
+    folder: 'vwanu/messages',
+    resource_type: 'auto',
+  },
+});
 const blogImages = new CloudinaryStorage({
   cloudinary,
   params: {
@@ -48,6 +57,7 @@ const albumImages = new CloudinaryStorage({
   },
 });
 
+export const messageStorage = multer({ storage: messageMedia });
 export const postStorage = multer({ storage: postImages });
 export const blogStorage = multer({ storage: blogImages });
 export const profilesStorage = multer({ storage: profilePictures });
