e6ccadb39cbaa310a41686f8b1f534cfe51498a2
Yves Gervens Constant <gervoos@gmail.com>
2022-09-15 14:53:37 -0400

    fix(messages): add emoji
    add emoji

--- client/src/features/chat/component/message/InputMessage.js
+++ client/src/features/chat/component/message/InputMessage.js
@@ -5,10 +5,14 @@ import TextareaAutosize from "react-autosize-textarea";
 // import * as Yup from "yup";
 import Loader from "../../../../components/common/Loader";
 import BoxGif from "../../../../components/common/BoxGif";
+import BoxEmoji from "../../../../components/common/BoxEmoji";
 import { AiOutlineCamera, AiOutlineVideoCamera, AiOutlineGif, AiOutlineDelete, AiOutlineSend } from "react-icons/ai";
 import { MdPhotoSizeSelectActual, MdVideoLibrary } from "react-icons/md";
+import { BsEmojiSmile } from "react-icons/bs";
+import { ImAttachment } from "react-icons/im";
 import { useCreateConversation, useCreateNewMessage } from "../../messageSlice";
 import InputPhoto from "../../../post/components/InputPhoto";
+import { useHover } from "@mantine/hooks";
 
 const InputMessage = ({ selectMember, type }) => {
   const [isLoading, setIsLoading] = useState(false);
@@ -18,9 +22,18 @@ const InputMessage = ({ selectMember, type }) => {
   const [selectedGif, setSelectedGif] = useState("");
   const [typeMedia, setTypeMedia] = useState("photo");
   const [message, setMessage] = useState("");
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+
+  const { hovered, ref } = useHover();
 
   const inputRef = useRef();
   const buttonRef = useRef();
+
+  const onEmojiClick = (event, emojiObject) => {
+    setChosenEmoji(emojiObject);
+    console.log(chosenEmoji);
+    setMessage(message + chosenEmoji?.emoji);
+  };
 
   const handleFocus = () => {
     inputRef.current.focus();
@@ -254,60 +267,69 @@ const InputMessage = ({ selectMember, type }) => {
           )}
 
           <div className="flex items-start py-0 px-2 bg-placeholder-color rounded-t-lg w-full">
-            <div className="flex justify-between text-left py-2 lg:py-2 lg:px-2">
-              <button
-                onClick={() => {
-                  if (selectedGif !== "") {
-                    // eslint-disable-next-line no-restricted-globals
-                    if (confirm("Are you sure you want to remove the gif?")) {
-                      setOpenUploadPhoto(!openUploadPhoto);
-                      setOpenUploadVideo(false);
-                      setSelectedGif("");
-                      setTypeMedia("photo");
-                    } else {
-                      setOpenUploadPhoto(true);
-                      setOpenUploadVideo(false);
-                      setSelectedGif("");
-                      setTypeMedia("photo");
-                    }
-                  } else {
-                    setOpenUploadPhoto(true);
-                    setOpenUploadVideo(false);
-                    setSelectedGif("");
-                    setTypeMedia("photo");
-                  }
-                }}
-                className="hover:text-primary inline-flex justify-center p-2 text-gray-900 rounded-lg cursor-pointer"
-              >
-                <AiOutlineCamera size={"24px"} />
-              </button>
-              <button
-                onClick={() => {
-                  if (selectedGif !== "") {
-                    // eslint-disable-next-line no-restricted-globals
-                    if (confirm("Are you sure you want to remove the gif?")) {
-                      setOpenUploadPhoto(false);
-                      setOpenUploadVideo(!openUploadVideo);
-                      setSelectedGif("");
-                      setTypeMedia("video");
-                    } else {
-                      setOpenUploadPhoto(false);
-                      setOpenUploadVideo(!openUploadVideo);
-                      setSelectedGif("");
-                      setTypeMedia("video");
-                    }
-                  } else {
-                    setOpenUploadPhoto(false);
-                    setOpenUploadVideo(!openUploadVideo);
-                    setSelectedGif("");
-                    setTypeMedia("video");
-                  }
-                }}
-                className="inline-flex justify-center p-2 text-gray-900 rounded-lg cursor-pointer hover:text-primary"
-              >
-                <AiOutlineVideoCamera size={"24px"} />
-              </button>
+            <div ref={ref} className="flex justify-between text-left py-2 lg:py-2 lg:px-2">
+              {hovered ? (
+                <>
+                  <button
+                    onClick={() => {
+                      if (selectedGif !== "") {
+                        // eslint-disable-next-line no-restricted-globals
+                        if (confirm("Are you sure you want to remove the gif?")) {
+                          setOpenUploadPhoto(!openUploadPhoto);
+                          setOpenUploadVideo(false);
+                          setSelectedGif("");
+                          setTypeMedia("photo");
+                        } else {
+                          setOpenUploadPhoto(true);
+                          setOpenUploadVideo(false);
+                          setSelectedGif("");
+                          setTypeMedia("photo");
+                        }
+                      } else {
+                        setOpenUploadPhoto(true);
+                        setOpenUploadVideo(false);
+                        setSelectedGif("");
+                        setTypeMedia("photo");
+                      }
+                    }}
+                    className="hover:text-primary inline-flex justify-center p-2 text-gray-900 rounded-lg cursor-pointer"
+                  >
+                    <AiOutlineCamera size={"24px"} />
+                  </button>
+                  <button
+                    onClick={() => {
+                      if (selectedGif !== "") {
+                        // eslint-disable-next-line no-restricted-globals
+                        if (confirm("Are you sure you want to remove the gif?")) {
+                          setOpenUploadPhoto(false);
+                          setOpenUploadVideo(!openUploadVideo);
+                          setSelectedGif("");
+                          setTypeMedia("video");
+                        } else {
+                          setOpenUploadPhoto(false);
+                          setOpenUploadVideo(!openUploadVideo);
+                          setSelectedGif("");
+                          setTypeMedia("video");
+                        }
+                      } else {
+                        setOpenUploadPhoto(false);
+                        setOpenUploadVideo(!openUploadVideo);
+                        setSelectedGif("");
+                        setTypeMedia("video");
+                      }
+                    }}
+                    className="inline-flex justify-center py-2 text-gray-900 rounded-lg cursor-pointer hover:text-primary"
+                  >
+                    <AiOutlineVideoCamera size={"24px"} />
+                  </button>
+                </>
+              ) : (
+                <div className="py-2">
+                  <ImAttachment size={"21px"} />
+                </div>
+              )}
               <BoxGif label={<AiOutlineGif size={"24px"} />} setSelectedGif={handleOpenGif} />
+              <BoxEmoji label={<BsEmojiSmile size={"21px"} />} setSelectedEmoji={onEmojiClick} />
             </div>
             <div className="w-full py-2">
               <TextareaAutosize
