d9a6c78c04e3ef8603874d35dd5df1b65987d9bd
Wadson <vavalson@hotmail.com>
2022-09-23 11:08:06 -0400

    perf(discussion): change discussion from sequelize.query to sequelize.literal

--- server/src/services/discussion/hooks/includeLastComment.ts
+++ server/src/services/discussion/hooks/includeLastComment.ts
@@ -1,51 +1,27 @@
 // import IncludeAssociations from '../../../Hooks/IncludeAssociations';
-import { QueryTypes } from '@sequelize/core';
-import { BadRequest } from '@feathersjs/errors';
-import getPaginationDetails from '../../../lib/utils/getPaginationDetails';
-import getQueryParameters from '../../../lib/utils/getQueryParameters';
-import getPaginatedResponse from '../../../lib/utils/getPaginatedResponse';
+
+import isEmpty from 'lodash/isEmpty';
 
 const IncludeLast = (single: boolean) => async (context) => {
-  const { app, params, service } = context;
-  const { query = {} } = params;
-  const stringQuery = getQueryParameters('D', 'DiscussionId', query);
-  let { $limit, $skip } = query;
-  let { max } = service.options.paginate;
-  const Sequelize = app.get('sequelizeClient');
-  const INTERNAL_MAX = 50;
-  max = Number.parseInt(max, 10) || INTERNAL_MAX;
-  $limit = Number.parseInt($limit, 10) || max;
-  $skip = Number.parseInt($skip, 10) || 0;
+  const { app, params } = context;
 
-  const { offsetAndLimit } = getPaginationDetails({ $limit, $skip, max });
-  const command = `SELECT 
-  "D"."id", 
-  "D"."title",
-  "D"."body",
-  "D"."banned",
-  "D"."bannedReason",
-  "D"."locked",
-  json_build_object(
-      'id',"U"."id",
-      'firstName',"U"."firstName",
-      'lastName',"U"."lastName",
-      'profilePicture',"U"."profilePicture",
-      'createdAt',"U"."createdAt",
-      'updatedAt',"U"."updatedAt"
-    ) AS "User", 
-    ( 
+  const Sequelize = app.get('sequelizeClient');
+
+  const activeParticipants = ` ( 
       SELECT 
       COUNT(DISTINCT "DCM"."UserId")
       FROM "Discussions" AS "DCM"
-      WHERE "DCM"."DiscussionId" = "D"."id"
-      )::int As "activeParticipants" ,
-    (
-      SELECT 
+      WHERE "DCM"."DiscussionId" = "Discussion"."id"
+      )::int
+    `;
+
+  const amountOfComments = `(   SELECT 
       COUNT(*) 
       FROM "Discussions" AS "DCC"
-      WHERE "DCC"."DiscussionId" = "D"."id"
-    )::int As "amountOfComments" , 
-    (
+      WHERE "DCC"."DiscussionId" = "Discussion"."id"
+    )::int`;
+
+  const lastComment = `(
     SELECT   
       json_build_object(
         'id',"DC"."id",
@@ -61,36 +37,47 @@ const IncludeLast = (single: boolean) => async (context) => {
       From "Discussions" AS "DC" 
       INNER JOIN "Users" AS "UC" ON "DC"."UserId" = "UC"."id"
       
-      WHERE "DC"."DiscussionId"="D"."id"
+      WHERE "DC"."DiscussionId"="Discussion"."id"
       ORDER BY "DC"."createdAt" ASC
       LIMIT 1
-    ) AS "lastComment"
-    From "Discussions" AS "D" 
-    INNER JOIN "Users" AS "U" ON "D"."UserId" = "U"."id"
-    ${single ? `WHERE "D"."id"='${context.id}'` : stringQuery}
-    GROUP BY "D"."id","D"."title", "D"."body","U"."firstName" ,"U"."id", "U"."lastName","U"."profilePicture","U"."createdAt","U"."updatedAt"
-    LIMIT ${single ? 1 : offsetAndLimit.limit} 
-    OFFSET ${single ? 0 : offsetAndLimit.offset}
-     `;
+    ) `;
 
-  try {
-    const discussionList = await Sequelize.query(command, {
-      type: QueryTypes.SELECT,
-    });
+  const { query: where } = context.app
+    .service(context.path)
+    .filterQuery(context.params);
 
-    context.result = single
-      ? discussionList[0]
-      : getPaginatedResponse({ $limit, $skip, data: discussionList, max });
-  } catch (error) {
-    if (error?.original?.code)
-      switch (error?.original?.code) {
-        case '42703':
-          throw new BadRequest('You requested a column that does not exists');
-        default:
-          throw new Error(error.message);
-      }
-    throw new Error(error.message);
-  }
+  // eslint-disable-next-line no-nested-ternary
+  const clause = single
+    ? { id: context.id }
+    : isEmpty(where)
+    ? { DiscussionId: null }
+    : { ...where };
+  params.sequelize = {
+    where: clause,
+    attributes: {
+      include: [
+        [Sequelize.literal(activeParticipants), 'activeParticipants'],
+        [Sequelize.literal(amountOfComments), 'amountOfComments'],
+        [Sequelize.literal(lastComment), 'lastComment'],
+      ],
+      exclude: ['DiscussionId', 'UserId', 'CommunityId'],
+    },
+    include: [
+      {
+        model: Sequelize.models.User,
+        attributes: [
+          'id',
+          'firstName',
+          'lastName',
+          'profilePicture',
+          'createdAt',
+          'updatedAt',
+        ],
+        required: true,
+      },
+    ],
+    raw: false,
+  };
 
   return context;
 };
