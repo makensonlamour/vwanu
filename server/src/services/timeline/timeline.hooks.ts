218ec51d0676a240c76d1077a61e78969ddfbc79
Wadson <vavalson@hotmail.com>
2022-09-23 20:07:48 -0400

    refactor(timeline): cleaned the code a bit and added getTimeline hook

--- server/src/services/timeline/timeline.hooks.ts
+++ server/src/services/timeline/timeline.hooks.ts
@@ -1,83 +1,16 @@
-// import { HooksObject } from '@feathersjs/feathers';
 import * as authentication from '@feathersjs/authentication';
 
 /** Local dependencies */
-import IncludeAssociations from '../../Hooks/IncludeAssociations';
-import addAssociation from '../../Hooks/AddAssociations';
-import OrderBy from '../../Hooks/OrderBy.hooks';
+import GetTimeline from './hooks/getTimeline';
 
 const { authenticate } = authentication.hooks;
 
-const onlyPublic = (context) => {
-  const { params } = context;
-  const { query } = params;
-  query.PostId = null;
-  query.CommunityId = null;
-  query.privacyType = 'public';
-  return context;
-};
 
-const UserAttributes = [
-  'firstName',
-  'lastName',
-  'id',
-  'profilePicture',
-  'createdAt',
-];
 export default {
   before: {
-    all: [
-      authenticate('jwt'),
-      IncludeAssociations({
-        include: [
-          {
-            model: 'posts',
-            as: 'Comments',
-            attributes: ['id', 'postText', 'PostId', 'updatedAt', 'createdAt'],
-            include: [
-              {
-                model: 'posts',
-                as: 'User',
-                attributes: UserAttributes,
-              },
-            ],
-          },
-          {
-            model: 'posts',
-            as: 'Media',
-            include: [
-              {
-                model: 'posts',
-                as: 'User',
-                attributes: UserAttributes,
-              },
-            ],
-          },
-
-          {
-            model: 'posts',
-            as: 'Reactions',
-            include: [
-              {
-                model: 'posts',
-                as: 'User',
-                attributes: UserAttributes,
-              },
-            ],
-          },
-        ],
-      }),
-      addAssociation({
-        models: [
-          {
-            model: 'users',
-            attributes: UserAttributes,
-          },
-        ],
-      }),
-    ],
-    find: [onlyPublic, OrderBy({ createdAt: -1 })],
-    get: [],
+    all: [authenticate('jwt')],
+    find: [GetTimeline],
+    get: [GetTimeline],
     create: [],
     update: [],
     patch: [],
