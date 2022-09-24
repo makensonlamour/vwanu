4b879fcfdc5be3b32ebfbd1356e78e32f7513b9b
Wadson Vaval <vavalson@hotmail.com>
2022-09-24 18:52:04 -0400

    feat(community): add pending invitation and pagination to community find (#405)

--- server/src/services/communities/hooks/findCommunities.ts
+++ server/src/services/communities/hooks/findCommunities.ts
@@ -1,22 +1,71 @@
-import { QueryTypes } from '@sequelize/core';
+import { Op } from '@sequelize/core';
 import { HookContext } from '@feathersjs/feathers';
-import { GeneralError } from '@feathersjs/errors';
 
 export default async (context: HookContext) => {
-  const { app } = context;
+  const { app, params } = context;
   const Sequelize = app.get('sequelizeClient');
 
+  //   const query2 = `SELECT
+  //   "C"."name",
+  //   "C"."description",
+  //   "C"."privacyType",
+  //   "C"."id",
+  //   "C"."UserId",
+  //   "C"."profilePicture",
+  //   "C"."coverPicture",
+  //   COUNT(DISTINCT CASE WHEN "CU"."CommunityId"="C"."id" THEN "CU"."UserId" END)::int As "amountOfMembers" ,
+  //   (SELECT
+  //     json_build_object(
+  //      'id', "CU"."UserId",
+  //      'role',"CR"."name",
+  //      'roleId',"CR"."id"
+  //     )
+  //     FROM "CommunityUsers" AS "CU"
+  //     INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
+  //     WHERE "CU"."CommunityId"="C"."id" and "CU"."UserId"='${context.params.User.id}'
+  //   ) as "IsMember",
 
-  const query2 = `SELECT 
-  "C"."name",
-  "C"."description", 
-  "C"."privacyType",
-  "C"."id",
-  "C"."UserId",
-  "C"."profilePicture",
-  "C"."coverPicture",
-  COUNT(DISTINCT CASE WHEN "CU"."CommunityId"="C"."id" THEN "CU"."UserId" END)::int As "amountOfMembers" , 
-  (SELECT 
+  // (SELECT
+  //   json_agg(
+  //     json_build_object('id', "U"."id",
+  //       'firstName',"U"."firstName",
+  //       'lastName',"U"."lastName",
+  //       'profilePicture',"U"."profilePicture",
+  //       'createdAt',"U"."createdAt",
+  //       'updatedAt',"U"."updatedAt",
+  //       'role',"CR"."name",
+  //       'roleId',"CR"."id"
+  //     )
+  //   )
+  //   FROM "CommunityUsers" AS "CU"
+  //   INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
+  //   INNER JOIN "Users" AS "U" ON "CU"."UserId" = "U"."id"
+  //   WHERE "CU"."UserId"="U"."id" AND "CU"."CommunityId"="C"."id"
+  //   LIMIT 10
+  //   )as "members",
+
+  // (SELECT
+  //   json_agg(
+  //     json_build_object(
+  //       'name',"I"."name",
+  //       'id',"I"."id"
+  //   )) FROM "Interests" AS "I"
+  //   INNER JOIN "Community_Interest" AS "CI" ON "CI"."InterestId" = "I"."id"
+  //   WHERE "CI"."CommunityId"="C"."id"
+  // )as "Interests"
+
+  // FROM "Communities" AS "C"
+  // INNER JOIN "CommunityUsers" as "CU" ON "CU"."CommunityId"="C"."id"
+  //  WHERE  "C"."privacyType" <> 'hidden' OR ("CU"."UserId"='${context.params.User.id}' AND "C"."privacyType" = 'hidden')
+  // GROUP BY "C"."name","C"."description", "C"."id"
+  // LIMIT 20
+  //   `;
+  const { query: where } = context.app
+    .service(context.path)
+    .filterQuery(context.params);
+
+  const isMember = `(
+  SELECT 
     json_build_object(
      'id', "CU"."UserId",
      'role',"CR"."name",
@@ -24,11 +73,21 @@ export default async (context: HookContext) => {
     ) 
     FROM "CommunityUsers" AS "CU"
     INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
-    WHERE "CU"."CommunityId"="C"."id" and "CU"."UserId"='${context.params.User.id}'
-  ) as "IsMember",
+    WHERE "CU"."CommunityId"="Community"."id" and "CU"."UserId"='${context.params.User.id}'
+  )`;
 
+  const Interests = `(
+SELECT 
+  json_agg(
+    json_build_object(
+      'name',"I"."name",
+      'id',"I"."id"
+  )) FROM "Interests" AS "I" 
+  INNER JOIN "Community_Interest" AS "CI" ON "CI"."InterestId" = "I"."id"
+  WHERE "CI"."CommunityId"="Community"."id"
+)`;
 
-(SELECT 
+  const members = `(SELECT 
   json_agg(
     json_build_object('id', "U"."id",
       'firstName',"U"."firstName",
@@ -43,36 +102,70 @@ export default async (context: HookContext) => {
   FROM "CommunityUsers" AS "CU" 
   INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
   INNER JOIN "Users" AS "U" ON "CU"."UserId" = "U"."id"
-  WHERE "CU"."UserId"="U"."id" AND "CU"."CommunityId"="C"."id"
+  WHERE "CU"."UserId"="U"."id" AND "CU"."CommunityId"="Community"."id"
   LIMIT 10
-  )as "members",
+  )`;
 
-(SELECT 
-  json_agg(
+  const amountOfMembers = `(
+    SELECT 
+    COUNT(DISTINCT "CU"."UserId")
+    FROM "CommunityUsers" AS "CU"
+    WHERE "CU"."CommunityId"="Community"."id")::int`;
+  // WHERE  "C"."privacyType" <> 'hidden' OR ("CU"."UserId"='${context.params.User.id}' AND "C"."privacyType" = 'hidden')
+
+  const isParticipant = `(
+    EXISTS (SELECT 1 FROM "CommunityUsers" AS "CU" WHERE "CU"."CommunityId"="Community"."id" AND "CU"."UserId"='${context.params.User.id}')
+  )`;
+  const pendingInvitation = `(
+    SELECT 
+    json_agg(
     json_build_object(
-      'name',"I"."name",
-      'id',"I"."id"
-  )) FROM "Interests" AS "I" 
-  INNER JOIN "Community_Interest" AS "CI" ON "CI"."InterestId" = "I"."id"
-  WHERE "CI"."CommunityId"="C"."id"
-)as "Interests"
+      'id', "INV"."id",
+      'role',"R"."name",
+      'roleId',"R"."id",
+      'createdAt',"INV"."createdAt",
+      'updatedAt',"INV"."updatedAt"
+      )
+      )
+     FROM "CommunityInvitationRequests" AS "INV" 
+     INNER JOIN "CommunityRoles" AS "R" ON "R"."id" = "INV"."CommunityRoleId"
+     WHERE "INV"."CommunityId"="Community"."id" AND "INV"."guestId"='${context.params.User.id}' AND "INV"."response" IS NULL
+  )`;
+  const clause = {
+    ...where,
+    [Op.and]: {
+      [Op.or]: [
+        { privacyType: { [Op.ne]: 'hidden' } },
+        {
+          [Op.and]: [
+            { privacyType: 'hidden' },
+            Sequelize.where(Sequelize.literal(isParticipant), true),
+          ],
+        },
+      ],
+    },
+  };
+  params.sequelize = {
+    where: clause,
+    logging: console.log,
+    attributes: {
+      include: [
+        [Sequelize.literal(isMember), 'IsMember'],
+        [Sequelize.literal(Interests), 'Interests'],
+        [Sequelize.literal(members), 'members'],
+        [Sequelize.literal(amountOfMembers), 'amountOfMembers'],
+        [Sequelize.literal(pendingInvitation), 'pendingInvitation'],
+      ],
+    },
 
-FROM "Communities" AS "C"
-INNER JOIN "CommunityUsers" as "CU" ON "CU"."CommunityId"="C"."id"
- WHERE  "C"."privacyType" <> 'hidden' OR ("CU"."UserId"='${context.params.User.id}' AND "C"."privacyType" = 'hidden')
-GROUP BY "C"."name","C"."description", "C"."id" 
-LIMIT 20
-  `;
+    raw: false,
+  };
 
+  // const communities = await Sequelize.query(query2, {
+  //   type: QueryTypes.SELECT,
+  // });
 
-  try {
-    const communities = await Sequelize.query(query2, {
-      type: QueryTypes.SELECT,
-    });
+  // context.result = communities;
 
-    context.result = communities;
-  } catch (error) {
-    throw new GeneralError(error);
-  }
   return context;
 };
