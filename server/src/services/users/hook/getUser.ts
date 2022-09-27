812503cac1d331778a93ed8cbc8feb9637c32b1f
Wadson Vaval <vavalson@hotmail.com>
2022-09-27 19:30:19 -0400

    feat(userfriends): add friend statues (#425)

--- server/src/services/users/hook/getUser.ts
+++ server/src/services/users/hook/getUser.ts
@@ -20,6 +20,28 @@ export default (context: HookContext) => {
        )
       )`;
 
+  const isFriend = `(
+        EXISTS(
+          SELECT 1 FROM "User_friends" WHERE ("User_friends"."UserId" = '${params.User.id}' AND "User_friends"."friendId" = "User"."id") OR ("User_friends"."UserId" = "User"."id" AND "User_friends"."friendId" = '${params.User.id}')
+        )
+  )`;
+  const hasReceivedFriendRequest = `(
+    EXISTS(
+    SELECT  1 FROM "User_friends_request" WHERE "User_friends_request"."UserId" ='${params.User.id}' AND "User_friends_request"."friendsRequestId" =  "User"."id" 
+      ))`;
+
+  const hasSentFriendRequest = `(
+    EXISTS(
+    SELECT  1 FROM "User_friends_request" WHERE ("User_friends_request"."friendsRequestId" = '${params.User.id}' AND "User_friends_request"."UserId" = "User"."id" )
+      ))`;
+
+  const attributes = {
+    include: [
+      [Sequelize.literal(isFriend), 'isFriend'],
+      [Sequelize.literal(hasReceivedFriendRequest), 'hasReceivedFriendRequest'],
+      [Sequelize.literal(hasSentFriendRequest), 'hasSentFriendRequest'],
+    ],
+  };
   delete where?.profilePrivacy;
   let clause = {
     ...where,
@@ -42,7 +64,9 @@ export default (context: HookContext) => {
   }
 
   params.sequelize = {
+    // logging: console.log,
     where: clause,
+    attributes,
   };
   return context;
 };
