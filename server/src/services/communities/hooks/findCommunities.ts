846667aba4c1fa64e01ce3aa9e6147a84b7690ff
Wadson <vavalson@hotmail.com>
2022-09-28 03:36:05 -0400

    feat(community filters): add filter by creator, popular, timecreated

--- server/src/services/communities/hooks/findCommunities.ts
+++ server/src/services/communities/hooks/findCommunities.ts
@@ -5,64 +5,26 @@ export default async (context: HookContext) => {
   const { app, params } = context;
   const Sequelize = app.get('sequelizeClient');
 
-  //   const query2 = `SELECT
-  //   "C"."name",
-  //   "C"."description",
-  //   "C"."privacyType",
-  //   "C"."id",
-  //   "C"."UserId",
-  //   "C"."profilePicture",
-  //   "C"."coverPicture",
-  //   COUNT(DISTINCT CASE WHEN "CU"."CommunityId"="C"."id" THEN "CU"."UserId" END)::int As "amountOfMembers" ,
-  //   (SELECT
-  //     json_build_object(
-  //      'id', "CU"."UserId",
-  //      'role',"CR"."name",
-  //      'roleId',"CR"."id"
-  //     )
-  //     FROM "CommunityUsers" AS "CU"
-  //     INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
-  //     WHERE "CU"."CommunityId"="C"."id" and "CU"."UserId"='${context.params.User.id}'
-  //   ) as "IsMember",
-
-  // (SELECT
-  //   json_agg(
-  //     json_build_object('id', "U"."id",
-  //       'firstName',"U"."firstName",
-  //       'lastName',"U"."lastName",
-  //       'profilePicture',"U"."profilePicture",
-  //       'createdAt',"U"."createdAt",
-  //       'updatedAt',"U"."updatedAt",
-  //       'role',"CR"."name",
-  //       'roleId',"CR"."id"
-  //     )
-  //   )
-  //   FROM "CommunityUsers" AS "CU"
-  //   INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
-  //   INNER JOIN "Users" AS "U" ON "CU"."UserId" = "U"."id"
-  //   WHERE "CU"."UserId"="U"."id" AND "CU"."CommunityId"="C"."id"
-  //   LIMIT 10
-  //   )as "members",
-
-  // (SELECT
-  //   json_agg(
-  //     json_build_object(
-  //       'name',"I"."name",
-  //       'id',"I"."id"
-  //   )) FROM "Interests" AS "I"
-  //   INNER JOIN "Community_Interest" AS "CI" ON "CI"."InterestId" = "I"."id"
-  //   WHERE "CI"."CommunityId"="C"."id"
-  // )as "Interests"
-
-  // FROM "Communities" AS "C"
-  // INNER JOIN "CommunityUsers" as "CU" ON "CU"."CommunityId"="C"."id"
-  //  WHERE  "C"."privacyType" <> 'hidden' OR ("CU"."UserId"='${context.params.User.id}' AND "C"."privacyType" = 'hidden')
-  // GROUP BY "C"."name","C"."description", "C"."id"
-  // LIMIT 20
-  //   `;
-  const { query: where } = context.app
+  const { filters, query: where } = context.app
     .service(context.path)
     .filterQuery(context.params);
+
+  let order = [];
+  if (filters.$sort) {
+    const { amountOfMembers } = filters.$sort;
+    const customFilters: any = {};
+    if (amountOfMembers) {
+      customFilters.amountOfMembers = amountOfMembers;
+      delete filters.$sort.amountOfMembers;
+    }
+    const allField = { ...filters.$sort, ...customFilters };
+    order = Object.keys(allField).map((key) => {
+      const descAsc = allField[key] === 1 ? 'ASC' : 'DESC';
+      if (customFilters[key])
+        return [[Sequelize.literal(`"${key}"`), `${descAsc}`]];
+      return [`${key}`, descAsc];
+    });
+  }
 
   const isMember = `(
   SELECT 
@@ -106,7 +68,7 @@ SELECT
   LIMIT 10
   )`;
 
-  const amountOfMembers = `(
+  const countAmountOfMembers = `(
     SELECT 
     COUNT(DISTINCT "CU"."UserId")
     FROM "CommunityUsers" AS "CU"
@@ -134,6 +96,7 @@ SELECT
      WHERE "INV"."CommunityId"="Community"."id" AND "INV"."guestId"='${context.params.User.id}' AND "INV"."response" IS NULL
   )`;
   const clause = {
+    // loging: console.log,
     ...where,
     [Op.and]: {
       [Op.or]: [
@@ -155,19 +118,14 @@ SELECT
         [Sequelize.literal(isMember), 'IsMember'],
         [Sequelize.literal(Interests), 'Interests'],
         [Sequelize.literal(members), 'members'],
-        [Sequelize.literal(amountOfMembers), 'amountOfMembers'],
+        [Sequelize.literal(countAmountOfMembers), 'amountOfMembers'],
         [Sequelize.literal(pendingInvitation), 'pendingInvitation'],
       ],
     },
-
+    include: { model: Sequelize.models.CommunityUsers, required: true },
+    order,
     raw: false,
   };
-
-  // const communities = await Sequelize.query(query2, {
-  //   type: QueryTypes.SELECT,
-  // });
-
-  // context.result = communities;
 
   return context;
 };
