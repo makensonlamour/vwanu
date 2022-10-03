7f0bba6f9c0468d631ed4b28b9f10ab306c6f194
Wadson <vavalson@hotmail.com>
2022-10-03 16:44:34 -0400

    feat(countries): add countries seeding

---
+++ server/migrations/seeders/20221003203311-countries.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { v4 } = require('uuid');
+const countries = require('../data/counties');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'State',
+      countries.map((country) => ({
+        name: country.name,
+        id: v4(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        initials: country.iso2,
+      }))
+    );
+  },
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('State', null, {});
+  },
+};
