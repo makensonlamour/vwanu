5699d6be9031315de53ffa0cd00dbb03a02a1422
Wadson <vavalson@hotmail.com>
2022-10-31 13:51:40 -0400

    feat(authentication): added support for google, twitter, facebook Oauth
    Allow user to login/signup via their existing google and facebook accounts

--- server/src/authentication.ts
+++ server/src/authentication.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-classes-per-file */
 /* eslint-disable no-unused-vars */
 import { ServiceAddons, Params } from '@feathersjs/feathers';
 import {
@@ -5,12 +6,18 @@ import {
   AuthenticationBaseStrategy,
   AuthenticationResult,
   JWTStrategy,
+  AuthenticationRequest,
 } from '@feathersjs/authentication';
 
 import { LocalStrategy } from '@feathersjs/authentication-local';
 import * as local from '@feathersjs/authentication-local';
 
-import { expressOauth } from '@feathersjs/authentication-oauth';
+import {
+  expressOauth,
+  OAuthStrategy,
+  OAuthProfile,
+} from '@feathersjs/authentication-oauth';
+import axios from 'axios';
 import { issueRefreshToken } from '@jackywxd/feathers-refresh-token';
 import { Application } from './declarations';
 
@@ -22,6 +29,60 @@ declare module './declarations' {
   }
 }
 
+class FacebookStrategy extends OAuthStrategy {
+  // eslint-disable-next-line class-methods-use-this
+  async getProfile(authResult: AuthenticationRequest, _params: Params) {
+    console.log('****** In Get Profile Data ******\n\n');
+    console.log('authResult', authResult);
+    console.log('Getting access token');
+    // This is the OAuth access token that can be used
+    // for Facebook API requests as the Bearer token
+    const accessToken = authResult.access_token;
+    console.log('accessToken', accessToken);
+    const { data } = await axios.get('https://graph.facebook.com/me', {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+      params: {
+        // There are
+        fields: 'id,name,email',
+      },
+    });
+
+    console.log('data', data);
+    return data;
+  }
+
+  async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
+    console.log('****** In Get Entity Data ******\n\n');
+    console.log('profile', profile);
+    // `profile` is the data returned by getProfile
+    const baseData = await super.getEntityData(profile, existing, params);
+
+    return {
+      ...baseData,
+      email: profile.email,
+    };
+  }
+}
+
+class GoogleStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
+    console.log('****** In Get Entity Data ******\n\n');
+    console.log({ profile, existing });
+    // this will set 'googleId'
+
+    const baseData = await super.getEntityData(profile, existing, params);
+
+    // this will grab the picture and email address of the Google profile
+    return {
+      ...baseData,
+      isVerified: true,
+      profilePicture: profile.picture,
+      email: profile.email,
+    };
+  }
+}
 class AnonymousStrategy extends AuthenticationBaseStrategy {
   // eslint-disable-next-line class-methods-use-this
   async authenticate(authentication: AuthenticationResult, params: Params) {
@@ -37,6 +98,8 @@ export default function (app: Application): void {
   authentication.register('jwt', new JWTStrategy());
   authentication.register('local', new LocalStrategy());
   authentication.register('anonymous', new AnonymousStrategy());
+  authentication.register('facebook', new FacebookStrategy());
+  authentication.register('google', new GoogleStrategy());
 
   app.use('/authentication', authentication);
   const service = app.service('authentication');
