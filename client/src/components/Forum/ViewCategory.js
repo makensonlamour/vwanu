ad03f3d59b97c8bf9f3227a56d31066863425a42
Yves Gervens Constant <gervoos@gmail.com>
2022-12-07 19:08:45 -0400

    Newfix/vwanu (#573)
    * fix(package.json): remove disable_eslint-plugin
    
    remove disable_eslint-plugin
    
    * fix(fix forum): fix forum

--- client/src/components/Forum/ViewCategory.js
+++ client/src/components/Forum/ViewCategory.js
@@ -23,7 +23,7 @@ const ViewCategory = ({ data }) => {
           {data?.description === "" ? <p className="py-5"></p> : <p className="line-clamp-2 text-sm font-normal">{data?.description}</p>}
         </div>
         <div className="p-4 flex items-end">
-          <p className="text-gray-500 text-sm font-[400] self-end">{format(new Date(data?.createdAt), "MMM dd, yyyy")}</p>
+          <p className="text-gray-500 text-sm font-[400] self-end">{data && format(new Date(data?.createdAt), "MMM dd, yyyy")}</p>
         </div>
       </Link>
     </>
