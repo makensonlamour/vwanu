af9f9fa1840a80b8e457da4e9c5d73c7c6cf6190
Wadson Vaval <vavalson@hotmail.com>
2023-01-27 16:17:12 -0400

    Fix/post allow community members only (#588)
    * fix(communities): fixed non members posting to community
    
    Only a community member can post in it.
    
    * refactor(accesscommunity): added a function instead or reapeating the same logic
    
    * test(community test): updated community test
    
    ---------
    
    Co-authored-by: Wadson Vaval <wadsonvaval@Wadsons-MacBook-Pro.local>

--- server/src/services/communities/hooks/accessCommunity.ts
+++ server/src/services/communities/hooks/accessCommunity.ts
@@ -1,6 +1,34 @@
 import { QueryTypes } from '@sequelize/core';
 import { BadRequest } from '@feathersjs/errors';
-import { HookContext } from '@feathersjs/feathers';
+import { HookContext, Id } from '@feathersjs/feathers';
+
+const hasAccess = (
+  userId: Id,
+  field:
+    | 'canInvite'
+    | 'canInPost'
+    | 'canInUploadDoc'
+    | 'canInUploadPhotos'
+    | 'canInUploadVideo'
+    | 'canMessageInGroup'
+): string =>
+  `(CASE 
+            WHEN "CR"."name"='admin' 
+             AND "CR"."id" = "CU"."CommunityRoleId" 
+             AND "CU"."UserId"='${userId}' THEN true
+            WHEN "C"."${field}" = 'E' 
+            AND "CU"."UserId"='${userId}' THEN true
+            WHEN "C"."${field}" = 'M'  
+             AND "CR"."id" = "CU"."CommunityRoleId" 
+             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
+             AND "CU"."UserId"='${userId}' THEN true
+           WHEN "C"."${field}" = 'A'  
+             AND "CR"."id" = "CU"."CommunityRoleId" 
+             AND "CR"."name"='admin'
+             AND "CU"."UserId"='${userId}' THEN true
+           ELSE false
+           END
+           )`;
 
 export default async (context: HookContext) => {
   const { app } = context;
@@ -14,7 +42,9 @@ export default async (context: HookContext) => {
              'roleId',"CR"."id"
               ) from "CommunityUsers" as "CU" 
             INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
-            WHERE "CU"."CommunityId"="C"."id" and "CU"."UserId"='${context.params.User.id}' AND "CU"."untilDate" IS NULL
+            WHERE "CU"."CommunityId"="C"."id" and "CU"."UserId"='${
+              context.params.User.id
+            }' AND "CU"."untilDate" IS NULL
               ) AS "IsMember",
               (
                 SELECT 
@@ -29,121 +59,53 @@ export default async (context: HookContext) => {
                     'guestId',"INV"."guestId")
                     ) FROM "CommunityInvitationRequests" AS "INV" 
                     INNER JOIN "CommunityRoles" AS "R" ON "R"."id" = "INV"."CommunityRoleId"
-                    WHERE "INV"."CommunityId"="C"."id" AND "INV"."guestId"='${context.params.User.id}' AND "INV"."response" IS NULL
+                    WHERE "INV"."CommunityId"="C"."id" AND "INV"."guestId"='${
+                      context.params.User.id
+                    }' AND "INV"."response" IS NULL
                     )AS "pendingInvitation",
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canInPost" = 'E' THEN true 
-            WHEN "C"."canInPost" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canInPost" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canUserPost",
+          ${hasAccess(context.params.User.id, 'canInPost')}AS "canUserPost",
+          ${hasAccess(context.params.User.id, 'canInvite')}AS "canUserInvite",
+          ${hasAccess(
+            context.params.User.id,
+            'canInUploadDoc'
+          )}AS "canUserUploadDoc",
 
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canInvite" = 'E' THEN true 
-            WHEN "C"."canInvite" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canInvite" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canUserInvite",
+           ${hasAccess(
+             context.params.User.id,
+             'canInUploadPhotos'
+           )}AS "canUserUploadPhotos",
 
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canInUploadDoc" = 'E' THEN true 
-            WHEN "C"."canInUploadDoc" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canInUploadDoc" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canUserUploadDoc",
-
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canInUploadPhotos" = 'E' THEN true 
-            WHEN "C"."canInUploadPhotos" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canInUploadPhotos" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canUserUploadPhotos",
-
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canInUploadVideo" = 'E' THEN true 
-            WHEN "C"."canInUploadVideo" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canInUploadVideo" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canUserUploadVideo",
-          (CASE 
-            WHEN "CR"."name"='admin' 
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-            WHEN "C"."canMessageInGroup" = 'E' THEN true 
-            WHEN "C"."canMessageInGroup" = 'M'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND ("CR"."name"='admin' OR "CR"."name"='moderator')
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           WHEN "C"."canMessageInGroup" = 'A'  
-             AND "CR"."id" = "CU"."CommunityRoleId" 
-             AND "CR"."name"='admin'
-             AND "CU"."UserId"='${context.params.User.id}' THEN true
-           ELSE false
-           END
-           ) AS "canMessageUserInGroup",
-
-          COUNT(DISTINCT CASE WHEN "CU"."CommunityId"='${context.id}' THEN "CU"."UserId" END) As "amountOfMembers",  
+        ${hasAccess(
+          context.params.User.id,
+          'canInUploadVideo'
+        )} AS "canUserUploadVideo",
+${hasAccess(
+  context.params.User.id,
+  'canMessageInGroup'
+)} AS "canMessageUserInGroup",
+  
+          COUNT(DISTINCT CASE WHEN "CU"."CommunityId"='${
+            context.id
+          }' THEN "CU"."UserId" END) As "amountOfMembers",  
           json_agg( 
             json_build_object(
               'id', "I"."id",
               'name',"I"."name"
           )) as "Interests"
           FROM "Communities" AS "C" 
-          INNER JOIN "CommunityUsers" AS "CU" ON "CU"."CommunityId" = '${context.id}'
+          INNER JOIN "CommunityUsers" AS "CU" ON "CU"."CommunityId" = '${
+            context.id
+          }'
           INNER JOIN "CommunityRoles" AS "CR" ON "CR"."id" = "CU"."CommunityRoleId"
-          INNER JOIN "Community_Interest" AS "CI" ON "CI"."CommunityId" = '${context.id}'
+          INNER JOIN "Community_Interest" AS "CI" ON "CI"."CommunityId" = '${
+            context.id
+          }'
           INNER JOIN "Interests" AS "I" ON "I"."id" = "CI"."InterestId"
-          WHERE "C"."id" = '${context.id}' AND ("C"."privacyType" = 'public' OR ("CU"."UserId" = '${context.params.User.id}' AND "CU"."untilDate" IS NULL AND "CU"."banned"=false))
+          WHERE "C"."id" = '${
+            context.id
+          }' AND ("C"."privacyType" = 'public' OR ("CU"."UserId" = '${
+    context.params.User.id
+  }' AND "CU"."untilDate" IS NULL AND "CU"."banned"=false))
           
            GROUP BY "C"."name","C"."description", "C"."id" ,"C"."privacyType" , "C"."profilePicture", "C"."coverPicture","CU"."CommunityId", "CU"."UserId","CU"."CommunityRoleId","CU"."banned","CU"."bannedDate","CR"."name","CR"."id"
            LIMIT 1`;
