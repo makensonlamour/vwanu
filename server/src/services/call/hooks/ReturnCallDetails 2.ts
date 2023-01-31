9251c37b9e40d34eb7a69da7bc8e9cb060f21b34
Wadson Vaval <wadsonvaval@Wadsons-MacBook-Pro.local>
2023-01-30 22:53:05 -0400

    fix(hooks): removed accidently created duplicate files

--- server/src/services/call/hooks/ReturnCallDetails 2.ts
+++
@@ -1,7 +0,0 @@
-import { HookContext } from '@feathersjs/feathers';
-
-export default (context: HookContext): HookContext => {
-  if (context.data.callDetails)
-    context.result.details = context.data.callDetails;
-  return context;
-};
