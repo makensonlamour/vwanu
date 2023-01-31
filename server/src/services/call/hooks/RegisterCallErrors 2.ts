bb523be927e794247e819a2feccf365a0c2d4436
Wadson Vaval <vavalson@hotmail.com>
2023-01-30 23:06:03 -0400

    fix(errors   when  bad request happened): fix bad reques unhandled er… (#590)
    * fix(errors   when  bad request happened): fix bad reques unhandled errors
    
    * fix(hooks): removed accidently created duplicate files
    
    ---------
    
    Co-authored-by: Wadson Vaval <wadsonvaval@Wadsons-MacBook-Pro.local>

--- server/src/services/call/hooks/RegisterCallErrors 2.ts
+++
@@ -1,7 +0,0 @@
-import { HookContext } from '@feathersjs/feathers';
-import Logger from '../../../lib/utils/logger';
-
-export default (context: HookContext): HookContext => {
-  Logger.error(context.error);
-  return context;
-};
