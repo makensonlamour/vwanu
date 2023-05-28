04c48777d11989c99e54f9e88f3470875e21f7ac
Wadson Vaval <vavalson@hotmail.com>
2023-05-28 18:35:19 -0400

    refactor(sendwelcome email): welcome email is sent via the notifier

--- server/src/services/users/hook/welcomeEmail.hook.ts
+++ server/src/services/users/hook/welcomeEmail.hook.ts
@@ -1,38 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import { HookContext } from '@feathersjs/feathers';
-import Notifier from '../../../lib/utils/notifier/not';
-import { EmailerService } from '../../../lib/utils/outReach';
-
+import notifier from '../../authmanagement/notifier';
 import Logger from '../../../lib/utils/logger';
+import { Application } from '../../../declarations';
 
 export default async (context: HookContext): Promise<HookContext> => {
   const { result, app } = context;
   if (!result) return context; // Error creating a user
-
   try {
-    const res = await app
-      .service('template')
-      ._find({ query: { snug: 'WelcomeSignup' }, paginate: false });
-    if (!res) {
-      Logger.error('Welcome template not found');
-      throw new Error('Welcome not found');
-    }
-
-    const notifierInstance = new Notifier(EmailerService());
-    const { email, activationKey, firstName, lastName, id } = result;
-
-    const to = email;
-    const templateId = res[0].id;
-    notifierInstance.sendTemplate(to, templateId, {
-      id,
-      firstName,
-      lastName,
-      activationKey,
-    });
-  } catch (error) {
-    Logger.error(error);
-    throw new Error(error);
+    await notifier(app as Application).notifier('WelcomeSignup', result, {});
+  } catch (e) {
+    Logger.error(e);
   }
-
   return context;
 };
