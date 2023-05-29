768b17a0540410901e48a8de0770f44c06fb98c2
Wadson Vaval <vavalson@hotmail.com>
2023-05-29 17:01:55 -0400

    fix(hooks): remove unused user's hook

--- server/src/services/users/hook/index.ts
+++ server/src/services/users/hook/index.ts
@@ -1,5 +1,4 @@
 export { default as GetUser } from './getUser';
-export { default as SendEmail } from './sendEmail';
 export { default as AddVisitor } from './addVisitor';
 export { default as SaveAddress } from './saveAddress';
 export { default as IncludeAddress } from './includeAddress';
