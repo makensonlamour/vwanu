db35616709f48fed9b89b52753f6f4e81d4c5ac9
Yves Gervens Constant <gervoos@gmail.com>
2023-07-04 09:10:08 -0400

    feat(remove post input): remove post input, add read more

---
+++ client/src/features/post/components/ReadMore.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import React, { useState } from "react";
+
+const ReadMore = ({ children }) => {
+  const text = children;
+  console.log(text, "test ok");
+  const [isReadMore, setIsReadMore] = useState(true);
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <p className="card-text pt-0 w-[100%] font-normal">
+      {isReadMore ? text?.slice(0, 150) : text}
+      <span onClick={toggleReadMore} className="text-primary cursor-pointer text-sm">
+        {isReadMore ? "...read more" : " show less"}
+      </span>
+    </p>
+  );
+};
+
+const Content = ({ children }) => {
+  return <ReadMore>{children}</ReadMore>;
+};
+
+export default Content;
