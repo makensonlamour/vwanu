0aea4f2a397341abbb4c5769c275572645763f46
Wadson Vaval <vavalson@hotmail.com>
2023-08-05 13:02:46 -0400

    feat(added patch an removed to workplace): added patched and removed to workplace (#645)

--- server/src/services/workplace/workplace.hooks.ts
+++ server/src/services/workplace/workplace.hooks.ts
@@ -1,9 +1,14 @@
 import * as authentication from '@feathersjs/authentication';
 
+import PatchWorkplace from './hooks/EditWorkplace.hook';
+import RemoveWorkplace from './hooks/RemoveWorkplace.hook';
+
 const { authenticate } = authentication.hooks;
 
 export default {
   before: {
     all: authenticate('jwt'),
+    patch: PatchWorkplace,
+    remove: RemoveWorkplace,
   },
 };
